39edeaeb8d1adb02dbbe474cd778f0ea7f335ff8
Yang <ROS_etyang@seattleschools.org>
2017-05-11 13:55:23 -0700

    first commit

---
+++ MazeClient.java
@@ -0,0 +1,8 @@
+//this class will contain the main method that will run everything
+
+public class MazeClient {
+   //the main method here will be what runs the game
+   public static void main(String[] args) {
+   
+   }   
+}
