69acd1525cbf892d5602de09647a1a4fcbc5f98b
realhenryzhuo <henry_zhuo@outlook.com>
2017-05-16 08:16:14 -0700

    Add files via upload

---
+++ MazeClientPs.java
@@ -0,0 +1,12 @@
+// Pseudocode for Class MazeClient
+
+// import java.util.*;
+
+/*
+class MazeClient{	
+	void main(String[] args){
+		StartGame s1 = new StartGame(); // Our game is an object. To play a game, we must create the object.
+		s1.Play(); // Object StartGame will have a method named "Play" that "starts" the game.
+	}
+}
+*/
