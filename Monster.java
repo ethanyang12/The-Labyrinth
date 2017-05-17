78d1b1a698d9b3c34ffeb218e24fa155f7f37406
realhenryzhuo <henry_zhuo@outlook.com>
2017-05-17 08:26:55 -0700

    Delete Monster.java

--- Monster.java
+++
@@ -1,7 +0,0 @@
-/* All monsters will have the methods Damage and Death. This interface is implemented into every 
-   monster class
-*/
-public interface Monster {
-   public int Damage(); 
-   public int Death();   
-}
