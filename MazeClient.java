e099ac79ea314f9f85099f87a80d1ea39c7c1f7a
realhenryzhuo <henry_zhuo@outlook.com>
2017-05-17 08:26:23 -0700

    Delete MazeClient.java

--- MazeClient.java
+++
@@ -1,8 +0,0 @@
-//this class will contain the main method that will run everything
-
-public class MazeClient {
-   //the main method here will be what runs the game
-   public static void main(String[] args) {
-   
-   }   
-}
