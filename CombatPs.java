03bf2406057ac771a11f58d158fbe38fe4c8b45c
realhenryzhuo <henry_zhuo@outlook.com>
2017-05-22 08:31:09 -0700

    Add files via upload

---
+++ CombatPs.java
@@ -0,0 +1,27 @@
+class Combat{
+	method monster(){
+		if (monster encountered){
+			monster attacks animation;
+			give player choice to dodge or attack;
+			player randomly will get hit or misses // see below
+			
+			/*
+			In order to keep the potions useful, the player will have
+			a guaranteed chance of being hit but only once.
+			*/
+		}
+		
+		if (monster is hit twice){
+			monster dies;
+			proceed onwards;
+		}
+	}
+	
+	int PlayerHealth(){
+		int i = 100;
+		if (monster attacks){
+			int i = - damage of monster
+		}
+		return i;
+	}
+}
