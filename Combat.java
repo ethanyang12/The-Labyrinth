2b54fe558e30799e95d06ca16cf65ae3fb5146e6
realhenryzhuo <henry_zhuo@outlook.com>
2017-05-30 08:28:35 -0700

    Add files via upload

---
+++ Combat.java
@@ -0,0 +1,14 @@
+public class Combat{
+   // takes in int Death from interface Monster 
+   public static monsterAttack(int Death){
+      if (){ // monster encountered
+		// monster attack pic
+		// player loses health
+		System.out.println("You've just been attacked.")
+      }
+      
+      if (Death == 0){
+		proceed onwards; // probably requires method
+	  }
+   }
+}
