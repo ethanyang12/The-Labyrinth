2b4f887052b7e04c95b4e40a826f77525833d4ac
benlemaster <benlemaster7@gmail.com>
2017-06-02 14:14:09 -0700

    Add files via upload

--- MazeLayout.java
+++ MazeLayout.java
@@ -2,39 +2,52 @@ import java.util.*;
 
 public class MazeLayout {
 
-   public static void main(String[] args) throws Exception{
-      choice();
+   public static void main(String[] args) throws Exception {
+      int progress = 0;
+      int[] z = new int[2]; 
+      int x = z[0];
+      int y = z[1];
+      choice(progress, x, y);
+   
    }
    //this method calls the monsters at certain points in the maze.
-   public static void choice() throws Exception {
-   
-      int progress = 0;
-      
+   public static String choice(int progress, int x, int y) throws Exception {
       Scanner console = new Scanner(System.in);
-      
-      System.out.println("Would you like to go right or left?");
-      
-      String ok = console.next();
-      
-      //you fight pt guy
-      while (progress <= 15) {
-      
-         if (progress == 2) {
-            //call pt guy
-            progress++;
+      while (progress <= 6){
+         System.out.println("Would you like to go right or left?");
+         String ok = console.next();  
+         if (ok.equals("right")) {
+            x++;
+            right(progress, x, y);
          }
-         
-         else if (progress == 4) {
-         //call LGates
-         progress++;
+         else if (ok.equals("left")){
+            y++;
+            left(progress, x, y);
          }
-         
-         else if (progress == 6) {
-         //call Mr. T
-            System.out.println("Filler for stuff");
-         }
-        progress++;
+         return(ok);
       }
+      return(null);
+   }
+   
+   public static void right(int progress, int x, int y) throws Exception {
+            
       
+      if (x == 2) {        
+         System.out.print("PT Guy");
+         Monster ptguy = new PTGuy();         
+         progress++;            
+
+      }
+         
+         
+      
+      
+      progress++;    
+      choice(progress, x, y);
+   }
+   
+   public static void left(int progress, int x, int y) throws Exception {
+      progress++;
+      choice(progress, x, y);
    }
 }
