8af75220712814b6c6ff7c852d190c62875c3df0
ethanyang12 <etyang1230@gmail.com>
2017-06-07 13:40:03 -0700

    Add files via upload

--- Combat.java
+++ Combat.java
@@ -1,14 +1,34 @@
-public class Combat{
-   // takes in int Death from interface Monster 
-   public static monsterAttack(int Death){
-      if (){ // monster encountered
-		// monster attack pic
-		// player loses health
-		System.out.println("You've just been attacked.")
-      }
-      
-      if (Death == 0){
-		proceed onwards; // probably requires method
-	  }
+import java.util.*;
+
+public class Combat {
+   private int death;
+   
+   //constructor to hold how many hits different monsters can take before they die
+   public Combat(int death) {
+      this.death = death;
    }
+   //an entire combat sequence between one monster and a player
+   public void monsterAttack(int damage, int playerhp, int monster) {
+      Scanner console = new Scanner(System.in);
+      while (death > 0) {
+         playerhp = playerhp - damage;
+         System.out.println("You've been attacked! The monster did " + damage + " damage to you.");
+         System.out.println("HP - " + playerhp);
+         System.out.println();
+         
+         System.out.println("Type ATTACK");
+         if (console.next().equals("ATTACK")) {
+            System.out.println("You've attacked the monster!");
+            System.out.println();
+            death--;
+         }
+      }
+      if (monster != 4) {
+         System.out.println("You've beat the monster! Time to move on...");
+         System.out.println();
+      }
+      else {
+         System.out.println("Congradulations!!! You have beat Mr. T and exited the maze");
+      }      
+   }   
 }
