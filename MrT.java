6b08a3c0b47d73c3ed8720eaf99b8d9344d157f1
ethanyang12 <etyang1230@gmail.com>
2017-06-13 13:51:09 -0700

    Add files via upload

--- MrT.java
+++ MrT.java
@@ -1,17 +1,19 @@
-/* Mr T Final Boss
-	implements the monster interface
-	does 50hp damage to the player
-	takes 4 hits to kill
+/* Mr. T boss
+   implements the monster interface
+   does 50hp of damage to the player
+   takes 4 hits to kill
 */
-
-public class MrT implements Monster{
+public class MrT implements Monster {
    public int Damage() {
       int damage = 50;
       return damage;
    }
-	
    public int Death() {
       int hp = 4;
       return hp;
+   }
+   public int Order() {
+      int appearancenumber = 4;
+      return appearancenumber;
    }
 }
